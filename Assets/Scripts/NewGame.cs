5acbdd411eff4247ed129cd5c6fe55e161332d16
Metal-Homer <metal-homer@web.de>
2018-01-11 21:09:34 +0100

    Revert "mist"
    This reverts commit c07341ff384a9459f38b5e9ad4757bc409c4329a.

--- Assets/Scripts/NewGame.cs
+++ Assets/Scripts/NewGame.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class NewGame : MonoBehaviour {
 
 	public void restartGame(){
-		SceneManager.LoadScene (0);
+		Application.LoadLevel (0);
 	}
 }
